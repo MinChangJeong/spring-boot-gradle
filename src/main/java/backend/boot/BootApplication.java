5a3280bb786d592f1316d11e63f5e6032f0bb9e7
= <=>
2022-09-20 20:16:00 +0900

    modified

--- src/main/java/backend/boot/BootApplication.java
+++ src/main/java/backend/boot/BootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootApplication {
 
 	public static void main(String[] args) {
+		// start spring boot project
 		SpringApplication.run(BootApplication.class, args);
 	}
 
