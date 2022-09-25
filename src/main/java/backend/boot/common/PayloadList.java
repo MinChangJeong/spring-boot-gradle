88ced37c1edb98f382c6f77ba06dc692c87f05d8
= <wjdalsckd777@naver.com>
2022-09-25 23:01:20 +0900

    add PayloadList.java

---
+++ src/main/java/backend/boot/common/PayloadList.java
@@ -0,0 +1,39 @@
+package backend.boot.common;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Component
+@Qualifier("payloadList")
+public class PayloadList<E> extends ArrayList<E>{
+
+    private static final long serialVersionUID = 1L;
+
+    public PayloadList() {
+        super();
+    }
+
+    @SuppressWarnings("unchecked")
+    public PayloadList(List<?> list) {
+        if (list != null) {
+            Iterator<?> it = list.iterator();
+            while (it.hasNext()) {
+                this.add((E) it.next());
+            }
+        }
+    }
+
+    public void set(E obj) {
+        super.add(obj);
+    }
+
+    public void set(E[] objs) {
+        for (E obj : objs) {
+            super.add(obj);
+        }
+    }
+}
