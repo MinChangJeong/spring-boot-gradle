bf84482ee8e667d35cc7d242603e6ed5dafb6440
= <wjdalsckd777@naver.com>
2022-10-01 22:51:45 +0900

    add SessionIntercepter

---
+++ src/main/java/backend/boot/common/intercepter/SessionIntercepter.java
@@ -0,0 +1,16 @@
+package backend.boot.common.intercepter;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SessionIntercepter implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request, HttpServletResponse response,
+            Object obj) throws Exception {
+        
+        return false;
+    }
+}
