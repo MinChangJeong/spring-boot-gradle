d377295985c7d4ed265b8cb9a3e4493b98d93411
= <wjdalsckd777@naver.com>
2022-10-05 11:08:33 +0900

    modified test

--- src/test/java/backend/boot/BootApplicationTests.java
+++ src/test/java/backend/boot/BootApplicationTests.java
@@ -1,10 +1,9 @@
 package backend.boot;
 
-import backend.boot.initial.BootApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = BootApplication.class)
+@SpringBootTest
 class BootApplicationTests {
 
 	@Test
